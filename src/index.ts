97f9de9805fa7c49f7f6751216f18de902e89a89
Arvin Salvador <arvinsalvador.official@gmail.com>
2018-09-20 16:05:32 +0800

    feat(boilerplate): added boilerplate core files

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+const { ApolloServer, gql } = require('apollo-server-koa');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+ 
+const app = new Koa();
+server.applyMiddleware({ app });
+ 
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
