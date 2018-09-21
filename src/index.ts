08892a2d4d5dff15a68d8753e7f9ed0c1d0c809f
djansyle <djansyledjans@gmail.com>
2018-09-21 09:22:05 +0800

    fix(boilerplate): fix missing dependency

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const Koa = require('koa');
-const { ApolloServer, gql } = require('apollo-server-koa');
+import * as Koa from 'koa';
+import { ApolloServer, gql } from 'apollo-server-koa';
 
 const typeDefs = gql`
   type Query {
@@ -10,14 +10,14 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
-  },
+  }
 };
 
-const server = new ApolloServer({typeDefs, resolvers});
- 
+const server = new ApolloServer({ typeDefs, resolvers });
+
 const app = new Koa();
 server.applyMiddleware({ app });
- 
+
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
 );
