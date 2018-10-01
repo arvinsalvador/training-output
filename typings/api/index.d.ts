82b6249bff5ceb790870bf51754a45f941566f69
Arvin Salvador <arvinsalvador.official@gmail.com>
2018-10-01 14:14:15 +0800

    feat(typings): added namespace API for Input and Output

---
+++ typings/api/index.d.ts
@@ -0,0 +1,13 @@
+declare namespace API {
+  namespace Input {
+    interface CreateAccountInput {
+      id: string
+      balance: number
+      availableBalance: number
+    }
+  }
+
+  namespace Output {
+
+  }
+}
