cd66d0ea4b693f45d3cc06efc933af693c9366cc
Arvin Salvador <arvinsalvador.official@gmail.com>
2018-10-02 10:36:12 +0800

    feat(class): added BalanceTypes class

---
+++ src/classes/balancetypes.ts
@@ -0,0 +1,10 @@
+const TYPES = {
+  VIRTUAL: `Virtual`,
+  RESERVE: `Reserve`
+};
+
+export default class BalanceTypes {
+  static get TYPES() {
+    return TYPES;
+  }
+}
--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,1 +1,4 @@
 export { default as Account } from './account';
+export { default as Balances } from './balances';
+
+export { default as BalanceType } from './balancetypes';
