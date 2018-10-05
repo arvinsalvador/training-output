1f4e19e4fa93847482df62496e6f2fc0cee5ea6a
Arvin Salvador <arvinsalvador.official@gmail.com>
2018-10-05 13:42:16 +0800

    test(Balances): moved account instance to global and enhanced  each method test

--- test/classes/balances.spec.ts
+++ test/classes/balances.spec.ts
@@ -5,14 +5,18 @@ import { Account, Balances } from './../../src/classes';
 import { InvalidRequestError } from '../../src/errors';
 
 describe('Balances class', () => {
+  let account;
+
+  beforeEach(() => {
+    account = new Account({ balance: 100, availableBalance: 50 });
+    account.save();
+  });
+
   describe('constructor', () => {
     let instance;
     let id;
-    let account;
     beforeEach(() => {
-      account = new Account({ balance: 50, availableBalance: 50 });
-      account.save();
-      instance = new Balances({ account: account.id, context: 'anyContext', balance: 100 }, 'any');
+      instance = new Balances({ account: account.id, context: 'a', balance: 100 }, 'Reserve');
       id = instance.id;
     });
 
@@ -25,7 +29,7 @@ describe('Balances class', () => {
     });
 
     it('should set account field', () => {
-      expect(instance).to.have.property('context', 'anyContext');
+      expect(instance).to.have.property('context', 'a');
     });
 
     it('should set balance field', () => {
@@ -33,24 +37,21 @@ describe('Balances class', () => {
     });
 
     it('should set type field', () => {
-      expect(instance).to.have.property('type', 'any');
+      expect(instance).to.have.property('type', 'Reserve');
     });
 
     describe('Given account does not exist', () => {
       it('should throw an error', () => {
         expect(() => {
-          new Balances({ account: uuid(), context: 'anyContext', balance: 0 }, 'any');
+          new Balances({ account: uuid(), context: 'a', balance: 0 }, 'Reserve');
         }).to.throw(InvalidRequestError);
       });
     });
 
     describe('Given context is exist', () => {
-      beforeEach(() => {
-        new Balances({ account: account.id, context: 'existContext', balance: 100 }, 'any');
-      });
       it('should throw an error', () => {
         expect(() => {
-          new Balances({ account: uuid(), context: 'existContext', balance: 0 }, 'any');
+          new Balances({ account: uuid(), context: 'a', balance: 0 }, 'Reserve');
         }).to.throw(InvalidRequestError);
       });
     });
@@ -58,7 +59,7 @@ describe('Balances class', () => {
     describe('Given balance is less than equal to 0', () => {
       it('should throw an error', () => {
         expect(() => {
-          new Balances({ account: uuid(), context: 'secondContext', balance: -5 }, 'any');
+          new Balances({ account: uuid(), context: 'b', balance: -5 }, 'any');
         }).to.throw(InvalidRequestError);
       });
     });
@@ -70,99 +71,82 @@ describe('Balances class', () => {
   });
 
   describe('getBalance', () => {
+    describe('Given balance does not exist', () => {
+      expect(() => {
+        Balances.getBalance(uuid(), 'abcd', 'any');
+      }).to.throw(InvalidRequestError);
+    });
+
     describe('Given balance exist', () => {
       let instance;
-      let account;
       let result;
       beforeEach(() => {
-        account = new Account({ balance: 50, availableBalance: 0 });
-        account.save();
-        instance = new Balances({ account: account.id, context: 'a', balance: 20 }, 'Reserve');
-        result = instance.save();
+        instance = new Balances({account: account.id, context: 'c', balance: 50 }, 'Reserve');
+        instance.save();
+        result = Balances.getBalance(account.id, 'c', 'Reserve');
       });
 
       it('should give the balance', () => {
-        expect(Balances.getBalance(account.id, 'a', 'Reserve'))
-          .to.deep.equals(result);
+        expect(result).to.deep.equals(instance);
       });
 
-      //TODO: does it need to expect the return value
-      //of the instance since it returned value?
-      it('should have a return value of the instance');
-    });
-
-    describe('Given balance does not exist', () => {
-      it('should throw an error');
+      //TODO: Need clarrifications in this it()
+      it('should return a value of the instance');
     });
   });
 
   describe('getBalanceInfo', () => {
     describe('Given balance exist', () => {
       let instance;
-      let account;
       let result;
-      let id;
       beforeEach(() => {
-        account = new Account({ balance: 50, availableBalance: 0 });
-        account.save();
-        instance = new Balances({ account: account.id, context: 'b', balance: 20 }, 'Reserve');
-        result = instance.save();
-        id = result.id;
+        instance = new Balances({account: account.id, context: 'd', balance: 50 }, 'Reserve');
+        instance.save();
+        result = Balances.getBalance(account.id, 'd', 'Reserve');
       });
 
       it('should give the balance info', () => {
-        expect(Balances.getBalanceInfo(id, 'Reserve'))
-          .to.deep.equals(result);
+        expect(result).to.deep.equals(instance);
       });
 
-      //TODO: does it need to expect the return value
-      //of the instance since it returned value?
-      it('should have a return value of the instance');
+      //TODO: Need clarrifications in this it()
+      it('should return a value of the instance');
     });
 
-    describe('Given balance info does not exist', () => {
+    describe('Given balance does not exist', () => {
       it('should return null');
     });
   });
 
   describe('getBalances', () => {
     describe('Given balance exist', () => {
-      let instance;
-      let account;
-      let id;
+      let result;
       beforeEach(() => {
-        account = new Account({ balance: 50, availableBalance: 0 });
-        account.save();
-        id = account.id;
-        instance = new Balances({ account: id, context: 'c', balance: 20 }, 'Reserve');
-        instance.save();
+        result = Balances.getBalances(account.id, 'Reserve');
       });
 
-      it('should give the balances info', () => {
-        expect(Balances.getBalances(id, 'Reserve')).to.be.an('array');
+      it('should give balances info', () => {
+        expect(result).to.be.an('array');
       });
 
-      //TODO: does it need to expect the return value of instance?
-      it('should have a return value of the instance');
+      //TODO: Need clarrifications in this it()
+      it('should return a value of the instance');
     });
 
-    describe('Given balances does not exist', () => {
+    describe('Given balance does not exist', () => {
       it('should return null');
     });
   });
 
   describe('update', () => {
     let instance;
-    let account;
     let result;
     let balanceInfo;
     beforeEach(() => {
-      account = new Account({ balance: 50, availableBalance: 0 });
-      account.save();
-      instance = new Balances({ account: account.id, context: 'd', balance: 20 }, 'Reserve');
+      instance = new Balances({ account: account.id, context: 'e', balance: 20 }, 'Reserve');
       result = instance.save();
-      result.update(50);
-      balanceInfo = Balances.getBalance(account.id, 'd', 'Reserve');
+      result.update(20);
+      balanceInfo = Balances.getBalance(account.id, 'e', 'Reserve');
     });
 
     it('should return value of instance', () => {
@@ -170,8 +154,8 @@ describe('Balances class', () => {
     });
   });
 
-  //TODO: Cannot access balanceDatabase to ensure record is deleted
   describe('deleteRecord', () => {
+    //TODO: Cannot access balanceDatabase to check if record is deleted
     it('should delete record from `balanceDatabase`');
   });
 });
