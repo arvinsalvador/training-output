159b5371a1967cd1869ba179ef6e0d42bb9d3d4f
Arvin Salvador <arvinsalvador.official@gmail.com>
2018-10-10 14:18:34 +0800

    fix(index): fixed app to sync Account Model before listening

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import * as Koa from 'koa';
 import { ApolloServer } from 'apollo-server-koa';
 import { resolvers, typeDefs } from './graphql/index';
+import { accountModel } from './models';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = new Koa();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
-);
+accountModel.sync().then(() => {
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+  );
+});
